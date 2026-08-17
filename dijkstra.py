7aa1001b5a3b528e4d098d0a8bd85af981376c30
Aruta Kochey <arutakochey@gmail.com>
2026-08-18 01:12:17 +0530

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -4,30 +4,22 @@ def dijkstra(graph, source):
     n = len(graph)
     dist = [math.inf] * n
     visited = [False] * n
-
     dist[source] = 0
-
     for k in range(n):
         u = -1
         min_dist = math.inf
-
         for i in range(n):
             if not visited[i] and dist[i] < min_dist:
                 min_dist = dist[i]
                 u = i
-
         if u == -1:
             break
-
         visited[u] = True
-
         for v in range(n):
             if graph[u][v] != math.inf and not visited[v]:
                 if dist[u] + graph[u][v] < dist[v]:
                     dist[v] = dist[u] + graph[u][v]
-
     return dist
-
 graph = [
     [0, 4, math.inf, math.inf, 8],
     [4, 0, 2, 5, math.inf],
@@ -35,13 +27,9 @@ graph = [
     [math.inf, 5, 1, 0, 3],
     [8, math.inf, 6, 3, 0]
 ]
-
 cities = ["A", "B", "C", "D", "E"]
-
 source = 0
-
 distances = dijkstra(graph, source)
-
 print("Shortest distances from", cities[source])
 for i in range(len(cities)):
     print(f"{cities[source]} -> {cities[i]} = {distances[i]}")
